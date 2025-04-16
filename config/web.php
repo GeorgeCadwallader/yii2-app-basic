f0900cacfc878cfdb2b1ab4b5000c89f75ac0eae
George Cadwallader <george@practically.io>
2025-04-16 18:08:15 +0100

    chore: enable pretty url in web config

--- config/web.php
+++ config/web.php
@@ -42,14 +42,9 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-            ],
         ],
-        */
     ],
     'params' => $params,
 ];
